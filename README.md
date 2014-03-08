09b941a25ffdd4536851762808f4f9901f705d17
Andy Shora <andyshora@gmail.com>
2014-03-08 12:31:56 +0000

    Rewrite of tasks.js, fixed output bugs

--- README.md
+++ README.md
@@ -17,40 +17,40 @@ grunt.loadNpmTasks('grunt-yslow');
 ## Config
 - jasmine
   - pages
-  	- src : The path to test
-  	- thresholds : optional overrides, same format as global thresholds in options
+  - src : The path to test
+  - thresholds : optional overrides, same format as global thresholds in options
   - options
-  	- thresholds : The global test thresholds which cause the task to pass/fail.
-	    - weight : The maximum page weight allowed.
-		- speed : The maximum load time of the page allowed.
-		- score : The minimum [YSlow performance score] (http://yslow.org/ruleset-matrix/) (out of 100) required.
-		- requests : The maximum number of requests the page is allowed to send on load.
+  - thresholds : The global test thresholds which cause the task to pass/fail.
+    - weight : The maximum page weight allowed.
+    - speed : The maximum load time of the page allowed.
+    - score : The minimum [YSlow performance score] (http://yslow.org/ruleset-matrix/) (out of 100) required.
+    - requests : The maximum number of requests the page is allowed to send on load.
 
 
 ## Example grunt config
 
 ```javascript
 yslow: {
-		options: {
-			thresholds: {
-				weight: 180,
-				speed: 1000,
-				score: 80,
-				requests: 15
-			}
-		},
-		pages: {
-			files: [
-				{
-					src: 'http://andyshora.com'
-				},
-				{
-					src: 'http://www.google.co.uk',
-					thresholds: {
-						weight: 100
-					}
-				}
-			]
-		}
+  options: {
+    thresholds: {
+      weight: 180,
+      speed: 1000,
+      score: 80,
+      requests: 15
+    }
+  },
+  pages: {
+    files: [
+      {
+        src: 'http://andyshora.com'
+      },
+      {
+        src: 'http://www.google.co.uk',
+        thresholds: {
+          weight: 100
+        }
+      }
+    ]
+  }
 }
 ```
--- tasks/tasks.js
+++ tasks/tasks.js
@@ -9,165 +9,118 @@
 
 module.exports = function(grunt) {
 
-	// node api
-	var fs	 = require('fs'),
-		path = require('path'),
-		status = null,
-		resultsArr = [];
+  // node api
+  var fs   = require('fs'),
+    path = require('path'),
+    resultsArr = [];
 
-	// npm lib
-	var phantomjs = require('grunt-lib-phantomjs').init(grunt);
-	var webpage = 'http://andyshora.com';
+  // npm lib
+  var phantomjs = require('grunt-lib-phantomjs').init(grunt);
 
-	var exec = require('child_process').exec;
-	var _ = grunt.util._;
+  var childProcess = require('child_process');
 
-
-
-	grunt.registerMultiTask('yslow', 'Run jasmine specs headlessly through PhantomJS.', function() {
-
-			// Merge task-specific options with these defaults.
-		var options = this.options({
-			version : '0.0.1',
-			timeout : 5000,
-			styles	: [],
-			specs	 : [],
-			helpers : [],
-			vendor	: [],
-			host		: '',
-			template : __dirname + '/jasmine/templates/DefaultRunner.tmpl',
-			templateOptions : {},
-			phantomjs : {},
-			junit: {}
-		});
-
-		//setup(options);
-
-		var urls = this.data.files;
-		var testCount = urls.length;
-		var logBufferArr = [], thresholdArr = [];
-
-		var temp = testCount;
-		while(temp--) {
-			logBufferArr.push('');
-			thresholdArr.push({
-				thresholdWeight: null,
-				thresholdRequests: null,
-				thresholdScore: null,
-				thresholdSpeed: null
-			});
-		}
-
-		logWrite('Testing ' + testCount + ' URLs...\n\n');
-
-		// This task is asynchronous.
-		var done = this.async();
-
-		// Reset status.
-		status = {failed: 0, passed: 0, total: 0, duration: 0};
-
-		var i = 0, cb = 0;
-		// Process each filepath in-order, this should be recursive
-		for (var key in urls) {
+  var _ = grunt.util._;
 
 
 
-			var data = urls[key];
+  grunt.registerMultiTask('yslow', 'Run jasmine specs headlessly through PhantomJS.', function() {
 
-			var url = data.src;
+    var urls = this.data.files;
+    var testCount = urls.length;
+    var logBufferArr = [], thresholdArr = [];
+    var options = this.options({});
 
+    var temp = testCount;
 
+    while(temp--) {
+      logBufferArr.push('');
+      thresholdArr.push({
+        thresholdWeight: null,
+        thresholdRequests: null,
+        thresholdScore: null,
+        thresholdSpeed: null
+      });
+    }
 
-			//var thresholdWeight =
-
-			var customThresholds = Object.prototype.hasOwnProperty.call(data, 'thresholds');
-
-			thresholdArr[i].thresholdWeight = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'weight') ? data.thresholds.weight : options.thresholds.weight;
-
-			thresholdArr[i].thresholdRequests = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'requests') ? data.thresholds.requests : options.thresholds.requests;
-
-			thresholdArr[i].thresholdScore = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'score') ? data.thresholds.score : options.thresholds.score;
-
-			thresholdArr[i].thresholdSpeed = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'speed') ? data.thresholds.speed : options.thresholds.speed;
-
-
-
-			var cmd = 'phantomjs node_modules/grunt-yslow/tasks/lib/yslow.js --info basic ' + url;
-			var cp = exec(cmd, [], function (err, stdout, stderr) {
+    logWrite('Testing ' + testCount + ' URLs, this might take a few moments...\n\n');
 
 
-				var results = JSON.parse(stdout);
+    // This task is asynchronous.
+    var done = this.async();
+
+    var createPhantomRunner = function(i) {
+      // console.log('createPhantomRunner', i);
+
+      var url = urls[i].src[0];
+      var data = urls[i];
+
+      // setup thresholds
+      var customThresholds = Object.prototype.hasOwnProperty.call(data, 'thresholds');
+      thresholdArr[i].thresholdWeight = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'weight') ? data.thresholds.weight : options.thresholds.weight;
+      thresholdArr[i].thresholdRequests = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'requests') ? data.thresholds.requests : options.thresholds.requests;
+      thresholdArr[i].thresholdScore = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'score') ? data.thresholds.score : options.thresholds.score;
+      thresholdArr[i].thresholdSpeed = customThresholds && Object.prototype.hasOwnProperty.call(data.thresholds, 'speed') ? data.thresholds.speed : options.thresholds.speed;
 
 
-				logBufferArr[cb] = logBuffer(logBufferArr[cb], _checkResult(thresholdArr[cb].thresholdRequests >= results.r, thresholdArr[cb].thresholdRequests+ 'requests', 'Requests: ' + results.r));
+      // creates a seperate scope for child variable
+      var cmd = 'phantomjs node_modules/grunt-yslow/tasks/lib/yslow.js --info basic ' + url;
+      var child = childProcess.exec(cmd, [], function (err, stdout, stderr) {
+          // console.log('inside, cmd', cmd, url);
 
-				logBufferArr[cb] = logBuffer(logBufferArr[cb], _checkResult(thresholdArr[cb].thresholdScore <= results.o, thresholdArr[cb].thresholdScore, 'YSlow score: ' + results.o + '/100'));
+          var results = JSON.parse(stdout);
 
-				logBufferArr[cb] = logBuffer(logBufferArr[cb], _checkResult(thresholdArr[cb].thresholdSpeed >= results.lt, thresholdArr[cb].thresholdSpeed+'ms', 'Page load time: ' + results.lt + 'ms'));
+          var str = '';
 
-				logBufferArr[cb] = logBuffer(logBufferArr[cb], _checkResult(thresholdArr[cb].thresholdWeight >= (results.w/1000), thresholdArr[cb].thresholdWeight+'kb', 'Page weight: ' + (results.w/1000) + 'kb'));
+          str = logBuffer(str, _checkResult(thresholdArr[i].thresholdRequests >= results.r, thresholdArr[i].thresholdRequests + 'requests', 'Requests: ' + results.r));
+          str = logBuffer(str, _checkResult(thresholdArr[i].thresholdScore <= results.o, thresholdArr[i].thresholdScore, 'YSlow score: ' + results.o + '/100'));
+          str = logBuffer(str, _checkResult(thresholdArr[i].thresholdSpeed >= results.lt, thresholdArr[i].thresholdSpeed + 'ms', 'Page load time: ' + results.lt + 'ms'));
+          str = logBuffer(str, _checkResult(thresholdArr[i].thresholdWeight >= (results.w/1000), thresholdArr[i].thresholdWeight + 'kb', 'Page weight: ' + (results.w/1000) + 'kb'));
 
+          grunt.log.ok('\n');
 
-					if (cb===testCount-1) {
-						for (var j in logBufferArr) {
+          if (str.match(/\[FAIL\]/g)) {
 
-							grunt.log.ok('\n');
+            grunt.log.error('\n\n-----------------------------------------------' +
+              '\nTest ' + (i+1) + ': ' + urls[i].src + ' \t  FAILED' +
+            '\n-----------------------------------------------\n\n');
 
-							if (logBufferArr[j].match(/\[FAIL\]/g)) {
+            grunt.log.error(str);
 
-								grunt.log.error('\n\n-----------------------------------------------' +
-									'\nTest ' + (cb+1) + ': ' + urls[cb].src + ' \t  FAILED' +
-								'\n-----------------------------------------------\n\n');
+          } else {
+            grunt.log.ok('\n\n-----------------------------------------------' +
+              '\nTest ' + (i+1) + ': ' + urls[i].src +
+            '\n-----------------------------------------------\n\n');
 
-								grunt.log.error(logBufferArr[j]);
+            grunt.log.ok(str);
+          }
 
-							} else {
-								grunt.log.ok('\n\n-----------------------------------------------' +
-									'\nTest ' + (cb+1) + ': ' + urls[cb].src +
-								'\n-----------------------------------------------\n\n');
+      });
+    };
 
-								grunt.log.ok(logBufferArr[j]);
-							}
+    for (var i=0; i<urls.length; i++) {
+      createPhantomRunner(i);
+    }
 
-							// write results buffer
+  });
 
-						}
-					}
+  var _checkResult = function(res, threshold, msg) {
+    if (res) {
+      return msg + ' [PASS]';
+    } else {
+      return msg + ' [FAIL] threshold is ' + threshold;
+    }
+  };
 
-					cb++;
+  function logWrite(text, isInline) {
 
+    text += (isInline ? '' : '\n');
+    grunt.log.ok(text);
+  }
 
-
-			});
-
-			i++;
-
-		}
-
-
-
-
-	});
-
-	var _checkResult = function(res, threshold, msg) {
-		if (res) {
-			return msg + ' [PASS]';
-		} else {
-			return msg + ' [FAIL] threshold is ' + threshold;
-		}
-	};
-
-	function logWrite(text, isInline) {
-
-		text += (isInline ? '' : '\n');
-		//status.log += text;
-		grunt.log.ok(text);
-	}
-
-	function logBuffer(str, text, isInline) {
-		text += (isInline ? '' : '\n');
-		return str + text;
-	}
+  function logBuffer(str, text, isInline) {
+    text += (isInline ? '' : '\n');
+    return str + text;
+  }
 
 
 
